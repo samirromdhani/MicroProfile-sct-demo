43b62b72dbef7284fc0a7b8e31c9d54412e60122
Samir Romdhani <samir.romdhani@rte-france.com>
2023-02-09 14:03:09 +0100

    MicroProfile:/ initial commit

---
+++ service-a/src/main/java/org/lfenergy/compas/sct/demo/SctdemoRestApplication.java
@@ -0,0 +1,11 @@
+package org.lfenergy.compas.sct.demo;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class SctdemoRestApplication extends Application {
+}
